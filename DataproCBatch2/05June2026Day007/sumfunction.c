9b5d1ba5a848ba705a9f30d9150f376072627d2d
prasanth-vaindam <vaindam.prasanth@gmail.com>
2026-06-05 17:23:21 +0530

    during class

---
+++ DataproCBatch2/05June2026Day007/sumfunction.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int sum(int x,int y){
+	return x+y;
+}
+
+int sub(int x, int y){
+	return x-y;
+}
+
+double div(int x, int y){
+	return (double)x/y;
+}
+
+int main()
+{
+	int a,b;
+	printf("Enter Two Numbers: ");
+	scanf("%d%d",&a,&b);
+	
+	int result = sum(a,b); // function call
+	
+	printf("The sum of %d and %d is %d\n",a,b, sum(a,b));
+	printf("The difference of %d and %d is %d\n",a,b, sub(a,b));
+	printf("The divsion of %d and %d is %lf\n",a,b,div(a,b));
+		
+}
+
+
